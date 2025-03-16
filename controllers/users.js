8d201ef9841f9547c0528654da6c8719649a253f
Patrick Kurt O. Villamer <pvillamer@gbox.adnu.edu.ph>
2025-03-16 23:31:03 +0800

    Add files via upload

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,8 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'; // Import UUID for unique IDs
 
-let users = [];
+let users = []; // Array to store users
 
-export const createUser = (req, res) => {
+export const createUser = (req, res) => { // Create a new user
     const user = req.body;
   
     users.push({ ...user, id: uuidv4() });
@@ -10,7 +10,7 @@ export const createUser = (req, res) => {
     res.send(`User with the name ${user.firstName} added to the database!`);
 };
 
-export const getUser = (req, res) => {
+export const getUser = (req, res) => { // Get a user by ID
     const { id } = req.params;
   
     const foundUser = users.find((user) => user.id === id);
@@ -18,7 +18,7 @@ export const getUser = (req, res) => {
     res.send(foundUser);
 };
 
-export const deleteUser = (req, res) => {
+export const deleteUser = (req, res) => { // Delete a user by ID
     const { id } = req.params;
   
     users = users.filter((user) => user.id !== id);
@@ -26,27 +26,27 @@ export const deleteUser = (req, res) => {
     res.send(`User with the id ${id} deleted from the database.`);
 };
 
-export const updateUser = (req, res) => {
-    const { id } = req.params;
-    const { firstName, lastName, Age } = req.body;
+export const updateUser = (req, res) => { //  Update a user by ID
+    const { id } = req.params; // Get the ID from the request parameters
+    const { firstName, lastName, Age } = req.body; // Get the updated user data from the request body
   
-    const user = users.find((user) => user.id === id);
+    const user = users.find((user) => user.id === id); // Find the user in the array by ID
   
-    if (firstName) {
+    if (firstName) { // If the first name is provided, update the user's first name
       user.firstName = firstName;
     }
   
-    if (lastName) {
+    if (lastName) { // If the last name is provided, update the user's last name
       user.lastName = lastName;
     }
   
-    if (Age) {
+    if (Age) { // If the Age is provided, update the user's Age
       user.Age = Age;
     }
   
-    res.send(`User with the id ${id} has been updated.`);
+    res.send(`User with the id ${id} has been updated.`); // Send a response to the client
 };
 
-export const getUsers = (req, res) => {
+export const getUsers = (req, res) => { // Get all users
     res.send(users);
 }
